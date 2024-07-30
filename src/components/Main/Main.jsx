11fb4b3b40e115d4c49c81cb78dfa194c0e70855
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-07-30 18:59:13 +0530

    feat: Complete frontend

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Main.css';
+import { assets } from '../../assets/assets';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, KK.</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Help me plan a game night with 5 friends for under $100</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Come up with a product name for a new app</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Frame a message for a potential client</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className='bottom-info'>
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy & Gemini Apps
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main
