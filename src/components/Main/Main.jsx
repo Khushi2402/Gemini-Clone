d6bde69ebd34cae055c2b3c9c4d69e6dddf2466b
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-07-31 12:39:08 +0530

    feat: Complete Generative AI functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,6 +14,9 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
+
+        {!showResult
+        ? <>
         <div className="greet">
           <p>
             <span>Hello, KK.</span>
@@ -38,6 +41,19 @@ const Main = () => {
             <img src={assets.bulb_icon} alt="" />
           </div>
         </div>
+        </>
+        : <div className="result">
+            <div className="result-title">
+                <img src={assets.user_icon} alt="" />
+                <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+                <p dangerouslySetInnerHTML={{__html: resultData}}></p>
+            </div>
+        </div>
+        }
+        
         <div className="main-bottom">
           <div className="search-box">
             <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
